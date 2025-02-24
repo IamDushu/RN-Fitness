e5c26504dad7a4105262584978684bf8ccea4cf5
IamDushu <hey.dushyanth@gmail.com>
2025-02-24 16:49:32 +0530

    addSet, updateSet & deleteSet

--- src/services/setService.ts
+++ src/services/setService.ts
@@ -1,4 +1,5 @@
-import { ExerciseSet } from "@/types/models";
+import { ExerciseSet, ExerciseWithSets } from "@/types/models";
+import * as Crypto from "expo-crypto";
 
 export const getSetTotalWeight = (set: ExerciseSet) => {
   return (set.weight || 0) * (set.reps || 0);
@@ -20,4 +21,34 @@ const getBestSetRep = (sets: ExerciseSet[]) => {
   return sets.reduce((bestSet: ExerciseSet | null, set) => {
     return (set?.reps || 0) > (bestSet?.reps || 0) ? set : bestSet;
   }, null);
+};
+
+export const createSet = (exerciseId: string) => {
+  const newSet: ExerciseSet = {
+    id: Crypto.randomUUID(),
+    exerciseId: exerciseId,
+  };
+
+  return newSet;
+};
+
+export const updateSet = (
+  set: ExerciseSet,
+  updatedFields: Pick<ExerciseSet, "reps" | "weight">
+) => {
+  const updatedSet = { ...set };
+
+  if (updatedFields.reps !== undefined) {
+    updatedSet.reps = updatedFields.reps;
+  }
+
+  if (updatedFields.weight !== undefined) {
+    updatedSet.weight = updatedFields.weight;
+  }
+
+  if (updatedSet.weight && updatedSet.reps) {
+    updatedSet.oneRM = updatedSet.weight * (36.0 / (37.0 - updatedSet.reps));
+  }
+
+  return updatedSet;
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,15 @@
-import { WorkoutWithExercises } from "@/types/models";
+import {
+  ExerciseSet,
+  ExerciseWithSets,
+  WorkoutWithExercises,
+} from "@/types/models";
 import { create } from "zustand";
 import * as Crypto from "expo-crypto";
 import { finishWorkout, newWorkout } from "@/services/workoutService";
 import { createExercise } from "@/services/exerciseService";
 import { immer } from "zustand/middleware/immer";
+import { createSet, updateSet } from "@/services/setService";
+import { current } from "immer";
 
 type State = {
   currentWorkout: WorkoutWithExercises | null;
@@ -14,6 +20,12 @@ type Actions = {
   startWorkout: () => void;
   finishWorkout: () => void;
   addExercise: (name: string) => void;
+  addSet: (exerciseId: string) => void;
+  updateSet: (
+    setId: string,
+    updatedFields: Pick<ExerciseSet, "reps" | "weight">
+  ) => void;
+  deleteSet: (setId: string) => void;
 };
 
 export const useWorkouts = create<State & Actions>()(
@@ -50,6 +62,49 @@ export const useWorkouts = create<State & Actions>()(
 
       set(({ currentWorkout }) => {
         currentWorkout?.exercises.push(newExercise);
+      });
+    },
+    addSet: (exerciseId) => {
+      const newSet = createSet(exerciseId);
+
+      set(({ currentWorkout }) => {
+        const exercise = currentWorkout?.exercises.find(
+          (e) => e.id === exerciseId
+        );
+
+        exercise?.sets.push(newSet);
+      });
+    },
+    updateSet: (setId, updatedFields) => {
+      set(({ currentWorkout }) => {
+        let setToUpdate = currentWorkout?.exercises
+          ?.flatMap((e) => e.sets)
+          .find((s) => s.id === setId);
+
+        if (!setToUpdate) {
+          return;
+        }
+
+        // this is wrong in immer
+        // setToUpdate = updateSet(current(setToUpdate), updatedFields);
+
+        Object.assign(
+          setToUpdate,
+          updateSet(current(setToUpdate), updatedFields)
+        );
+      });
+    },
+    deleteSet: (setId) => {
+      set(({ currentWorkout }) => {
+        const exercise = currentWorkout?.exercises?.find((exercise) =>
+          exercise.sets.some((set) => set.id === setId)
+        );
+
+        if (!exercise) {
+          return;
+        }
+
+        exercise.sets = exercise?.sets.filter((s) => s.id !== setId);
       });
     },
   }))
